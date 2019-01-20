3ef1b62a92326a52c9a22a0184181e31397927fb
justcallmebeef <bethanymitch10@gmail.com>
2019-01-20 14:26:18 -0700

    added home get request

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express()
 const queries = require('./queries')
 const port = process.env.PORT || 3002 
 
+
+app.get('/', (req, res) => {
+    res.send('Please add climbers to end of URL')
+})
+
 app.get('/climbers', (req, res) => {
     queries.listAllClimbers().then(climbers => res.send(climbers))
 })
