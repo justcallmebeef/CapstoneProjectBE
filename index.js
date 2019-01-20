2abe4d5f5f346bec696b3692ae2309e31200cf21
justcallmebeef <bethanymitch10@gmail.com>
2019-01-20 13:15:08 -0700

    created a get request that pulls in the git all climbers query to get the data

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express') 
 const app = express() 
+const queries = require('./queries')
 const port = process.env.PORT || 3002 
 
-app.get('/', (req, res) => {
-    res.send("working!")
+app.get('/climbers', (req, res) => {
+    queries.listAllClimbers().then(climbers => res.send(climbers))
 })
 
 app.listen(port)
