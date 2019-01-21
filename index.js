b2dc47177a4c988d174b390ffb149ff54a9dcb62
justcallmebeef <bethanymitch10@gmail.com>
2019-01-20 17:02:21 -0700

    added morgan, cors, and body-parser and used them

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express') 
 const app = express() 
 const queries = require('./queries')
+const morgan = require('morgan')
+const bodyParser  = require('body-parser')
+const cors = require('cors')
 const port = process.env.PORT || 3002 
+
+app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
+app.use(bodyParser.json())
+app.use(cors)
 
 app.get('/', (req, res) => {
     res.send('Please add climbers to end of URL')
