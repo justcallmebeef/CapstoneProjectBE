4853505afa70a77d02df3424313ad51b6327b4bd
justcallmebeef <bethanymitch10@gmail.com>
2019-01-22 13:43:57 -0700

    removed commented code

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.get('/climbers', (req, res, next) => {
     queries.listAllClimbers().then(climbers => res.send(climbers)).catch(next)
 })
 
-// The following 2 `app.use`'s MUST follow ALL your routes/middleware
 app.use(notFound)
 app.use(errorHandler)
 
@@ -26,7 +25,6 @@ function notFound(req, res, next) {
   res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
 }
 
-// eslint-disable-next-line
 function errorHandler(err, req, res, next) {
   console.error('ERROR', err)
   const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
